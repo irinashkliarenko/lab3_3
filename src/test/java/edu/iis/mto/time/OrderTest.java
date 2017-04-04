7cec1bf8601f37e150e5d28c80d1dfb1d44deb64
Iryna Shkliarenko <irina.shkliarenko@gmail.com>
2017-04-04 15:39:13 +0200

    OrderTest class added

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,19 @@
+package edu.iis.mto.time;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrderTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
